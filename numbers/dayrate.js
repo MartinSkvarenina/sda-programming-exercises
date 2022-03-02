6a5f20aa5797bba08e0b10ad4c02c6d2723a89f8
Martin Škvarenina <martinskvarenina@MacBook-Pro-uzivatela-Martin.local>
2022-03-02 14:26:43 +0100

    zmena v subore dayrate.js

--- numbers/dayrate.js
+++ numbers/dayrate.js
@@ -1,3 +1,11 @@
 // Napište funkci pro výpočet denní sazby vzhledem k hodinové sazbě.
 // Funkce bude přijímat hodinovou sazbu pracovníka a vrátí cenu pracovníka za den, když počítáme s 8hodinovou pracovní dobou.
 // Vstup: 89, Výstup: 712
+
+
+const vypocetMzdy = (hodinovaSazdba, pracovnaDoba) => {
+    return hodinovaSazdba * pracovnaDoba
+    
+};
+let vysledok = vypocetMzdy(89, 8);
+console.log(vysledok);
--- numbers/daysInBudget.js
+++ numbers/daysInBudget.js
@@ -1,4 +1,4 @@
 // Napište funkci pro výpočet počtu pracovních dnů při pevně daném rozpočtu.
 // Funkce bude přijímat rozpočet zaměstnavatele a hodinovou sazbu pracovníka
 // a vrátí maximální počet dní po které si může zaměstnavatel pracovníka dovolit, když počítáme s 8hodinovou pracovní dobou.
-// Vstup: 20000, 89, Výstup: 28
+// Vstup: 20000, 89, 8 ; Výstup: 28
